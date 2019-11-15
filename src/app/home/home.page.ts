1cae8a3b9ce4357f945cc7568d1aebd51dea8b38
Thaís Oliveira <jts_oliveira@outlook.com>
2019-11-14 21:11:41 -0300

    Calculo basico

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public formData: any = {};
+
+  private expectativaVida: any = {
+    'f': 82,
+    'm': 76
+  };
+  private idadeAposentadoria: any = {
+    'f': 62,
+    'm': 65
+  };
+
+  constructor(private alertController: AlertController) {}
+
+  calcular(form: NgForm) {
+    if (form.valid) {
+      let tempoRecebimentoAnos: number = this.expectativaVida[this.formData.sexo] - this.idadeAposentadoria[this.formData.sexo];
+      let tempoRecebimentoMeses: number = tempoRecebimentoAnos*12;
+      let montanteInvestimento: number = tempoRecebimentoMeses*this.formData.renda;
+      let tempoParaPagarAnos: number = this.idadeAposentadoria[this.formData.sexo] -    this.formData.idade;
+      let tempoParaPagarMeses: number = tempoParaPagarAnos*12;
+      let valorContribuicaoMensal: number = montanteInvestimento / tempoParaPagarMeses;
+      this.presentAlert('Resultado', 'Contribuição mensal', 'Você deve poupar R$'+valorContribuicaoMensal.toFixed(2)+' ao mês');
+    }
+  }
+
+  async presentAlert(header, title, msg) {
+    const alert = await this.alertController.create({
+      header: header,
+      subHeader: title,
+      message: msg,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 
 }
