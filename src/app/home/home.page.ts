3adb8322289cc2da34b88e21e710b6469eff9688
Thaís Oliveira <jts_oliveira@outlook.com>
2019-11-21 06:45:41 -0300

    Calculando valor acumulado com VP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,27 +12,56 @@ export class HomePage {
   public formData: any = {};
 
   private expectativaVida: any = {
-    'f': 82,
-    'm': 76
+    'f': 22.5,
+    'm': 16.9
   };
   private idadeAposentadoria: any = {
     'f': 62,
     'm': 65
+  };
+  private rendimento: any = {
+    '1': 0.0657,
+    '2': 0.1036
   };
 
   constructor(private alertController: AlertController) {}
 
   calcular(form: NgForm) {
     if (form.valid) {
-      let tempoRecebimentoAnos: number = this.expectativaVida[this.formData.sexo] - this.idadeAposentadoria[this.formData.sexo];
-      let tempoRecebimentoMeses: number = tempoRecebimentoAnos*12;
-      let montanteInvestimento: number = tempoRecebimentoMeses*this.formData.renda;
-      let tempoParaPagarAnos: number = this.idadeAposentadoria[this.formData.sexo] -    this.formData.idade;
-      let tempoParaPagarMeses: number = tempoParaPagarAnos*12;
-      let valorContribuicaoMensal: number = montanteInvestimento / tempoParaPagarMeses;
+      let taxa: number = (Math.pow((this.rendimento[this.formData.risco]+1), 1/12)-1);
+      let mesesRecebimento: number = this.expectativaVida[this.formData.sexo]*12;
+      let renda: number = parseFloat(this.formData.aposentadoriaDesejada)
+      let valorAcumulado = this.PV(taxa, mesesRecebimento, renda);
+      let mesesPagamento: number = (this.idadeAposentadoria[this.formData.sexo] - this.formData.idade) * 12;
+      let valorContribuicaoMensal = this.PGTO(valorAcumulado, mesesPagamento, taxa);
+      console.log(valorContribuicaoMensal);
+      // let tempoParaPagarMeses: number = (this.idadeAposentadoria[this.formData.sexo] -    this.formData.idade)*12;
+      // let valorContribuicaoMensal: number = montanteInvestimento / tempoParaPagarMeses;
       this.presentAlert('Resultado', 'Contribuição mensal', 'Você deve poupar R$'+valorContribuicaoMensal.toFixed(2)+' ao mês');
     }
   }
+
+  private PV(rate: number, nper: number, pmt: number)
+  {
+    return pmt / rate * (1 - Math.pow(1 + rate, -nper));
+  }
+
+  private PGTO(valor: number, prestacoes: number, juros: number)
+    {
+        let E: number = 1;
+        let cont: number = 1;
+        juros = juros / 100;
+
+        for (let k = 1; k <= prestacoes; k++)
+        {
+            cont = cont * (1 + juros);
+            E = E + cont;
+        }
+        E = E - cont;
+        valor = valor * cont;
+
+        return valor /E;
+    }
 
   async presentAlert(header, title, msg) {
     const alert = await this.alertController.create({
